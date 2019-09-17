6ff081468388eef2b252a06bdfea9550c09996b8
Eibiel Sardjanto <eibiel.sardjanto@gmail.com>
2019-09-17 23:00:36 +0700

    - Initial Commit - Implemented Navigation Controller and Tableview Controller - Added Custom Cell xib

---
+++ makuy/Homescreen/CustomCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomCell.swift
+//  makuy
+//
+//  Created by Eibiel Sardjanto on 17/09/19.
+//  Copyright © 2019 Eibiel Sardjanto. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+}
